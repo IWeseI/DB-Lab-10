1f3f662b9728d1e698cd4337d7ae1be36a95a42f
IWeseI <57014490+IWeseI@users.noreply.github.com>
2021-11-24 22:47:09 -0500

    Add files via upload

---
+++ Lab10.py
@@ -0,0 +1,37 @@
+import glob
+import face_recognition
+from queue import PriorityQueue
+
+images = glob.glob("C:/Users/Wese/Downloads/lfw/*")
+images = images[:100]
+def range_search(image_name, r):
+    image = face_recognition.load_image_file(image_name)
+    image_encoding = face_recognition.face_encodings(image)[0]
+    image_encoding = [image_encoding]
+    result = []
+    for i in images:
+        image_compare = face_recognition.load_image_file(i)
+        if len(face_recognition.face_encodings(image_compare))>0:
+            image_compare_encoding = face_recognition.face_encodings(image_compare)[0]
+            dist = face_recognition.face_distance(image_encoding, image_compare_encoding)
+            if dist < r:
+                result.append(i)
+    return result
+
+def knn_search(image_name, k):
+    image = face_recognition.load_image_file(image_name)
+    image_encoding = face_recognition.face_encodings(image)[0]
+    image_encoding = [image_encoding]
+    result = PriorityQueue()
+    for i in images:
+        image_compare = face_recognition.load_image_file(i)
+        if len(face_recognition.face_encodings(image_compare))>0:
+            image_compare_encoding = face_recognition.face_encodings(image_compare)[0]
+            dist = face_recognition.face_distance(image_encoding, image_compare_encoding)
+            result.put((dist, i))
+    result_final = []
+    for i in range(k):
+        result_final.append(result.get())
+    return result_final
+
+print(knn_search("C:/Users/Wese/Downloads/lfw/Aaron_Eckhart_0001.jpg", 5))
